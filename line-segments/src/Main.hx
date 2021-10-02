4f436bce7735e3c95c853cd9ea5927cad9c4f650
Sylvio Sell <info@maitag.de>
2021-10-03 00:54:31 +0200

    codecleaning

--- line-segments/src/Main.hx
+++ line-segments/src/Main.hx
@@ -30,8 +30,6 @@ class Main extends Application
 	// ------------------------------------------------------------
 	var buffer:Buffer<LineSegment>;
 	var lineSegment:LineSegment;
-	var xStart:Int = 0;
-	var yStart:Int = 0;
 	
 	var isInit = false;
 	
@@ -46,77 +44,54 @@ class Main extends Application
 		peoteView.addDisplay(display);
 		display.addProgram(program);
 
-		xStart = Std.int(window.width / 2);
-		yStart = Std.int(window.height / 2);
 		
 		lineSegment = new LineSegment();
-		lineSegment.x = xStart;
-		lineSegment.y = yStart;
+		
+		// line starts at middle of the window
+		lineSegment.x = Std.int(window.width / 2);
+		lineSegment.y = Std.int(window.height / 2);
 		lineSegment.h = 3;
+		
+		// pivot point for rotation and positionizing
 		lineSegment.px = 0;
 		lineSegment.py = 1;
 		
 		buffer.addElement(lineSegment);
+		
 		isInit = true;
 	}
 	
-	// ------------------------------------------------------------
-	// ----------------- LIME EVENTS ------------------------------
-	// ------------------------------------------------------------	
-
-	override function onPreloadComplete():Void {
-		// access embeded assets from here
-	}
-
-	override function update(deltaTime:Int):Void {
-		// for game-logic update
-	}
-
-	// override function render(context:lime.graphics.RenderContext):Void {}
-	// override function onRenderContextLost ():Void trace(" --- WARNING: LOST RENDERCONTEXT --- ");		
-	// override function onRenderContextRestored (context:lime.graphics.RenderContext):Void trace(" --- onRenderContextRestored --- ");		
-
 	// ----------------- MOUSE EVENTS ------------------------------
 	override function onMouseMove (x:Float, y:Float):Void {
-		if (isInit) {
-			var a = xStart - x;
-			var b = yStart - y;
+		if (isInit) 
+		{
+			var a = lineSegment.x - x;
+			var b = lineSegment.y - y;
+			
 			lineSegment.w = Std.int( Math.sqrt( a * a + b * b ) );
 			lineSegment.r = Math.atan( a / b ); // WHAT THE ..... FUUUUCKKKK :;) -> needs radians!
+			
 			buffer.updateElement(lineSegment);
 		}
-	}	
+	}
+	
+	// -------------- WINDOWS EVENTS ----------------------------
+	override function onWindowResize (width:Int, height:Int):Void {
+		if (isInit) 
+		{	// keep line starting at middle of the window on resize
+			lineSegment.x = Std.int(width / 2);
+			lineSegment.y = Std.int(height / 2);
+		}
+	}
+	
 	// override function onMouseDown (x:Float, y:Float, button:lime.ui.MouseButton):Void {}	
 	// override function onMouseUp (x:Float, y:Float, button:lime.ui.MouseButton):Void {}	
 	// override function onMouseWheel (deltaX:Float, deltaY:Float, deltaMode:lime.ui.MouseWheelMode):Void {}
 	// override function onMouseMoveRelative (x:Float, y:Float):Void {}
-
-	// ----------------- TOUCH EVENTS ------------------------------
-	// override function onTouchStart (touch:lime.ui.Touch):Void {}
-	// override function onTouchMove (touch:lime.ui.Touch):Void	{}
-	// override function onTouchEnd (touch:lime.ui.Touch):Void {}
 	
 	// ----------------- KEYBOARD EVENTS ---------------------------
 	// override function onKeyDown (keyCode:lime.ui.KeyCode, modifier:lime.ui.KeyModifier):Void {}	
 	// override function onKeyUp (keyCode:lime.ui.KeyCode, modifier:lime.ui.KeyModifier):Void {}
 
-	// -------------- other WINDOWS EVENTS ----------------------------
-	override function onWindowResize (width:Int, height:Int):Void {
-		xStart = Std.int(width / 2);
-		yStart = Std.int(height / 2);
-	}
-	// override function onWindowLeave():Void { trace("onWindowLeave"); }
-	// override function onWindowActivate():Void { trace("onWindowActivate"); }
-	// override function onWindowClose():Void { trace("onWindowClose"); }
-	// override function onWindowDeactivate():Void { trace("onWindowDeactivate"); }
-	// override function onWindowDropFile(file:String):Void { trace("onWindowDropFile"); }
-	// override function onWindowEnter():Void { trace("onWindowEnter"); }
-	// override function onWindowExpose():Void { trace("onWindowExpose"); }
-	// override function onWindowFocusIn():Void { trace("onWindowFocusIn"); }
-	// override function onWindowFocusOut():Void { trace("onWindowFocusOut"); }
-	// override function onWindowFullscreen():Void { trace("onWindowFullscreen"); }
-	// override function onWindowMove(x:Float, y:Float):Void { trace("onWindowMove"); }
-	// override function onWindowMinimize():Void { trace("onWindowMinimize"); }
-	// override function onWindowRestore():Void { trace("onWindowRestore"); }
 	
 }
