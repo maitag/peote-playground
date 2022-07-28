99b3e30a6e41ddb70382e5ceef1fee8814d7e4fa
Sylvio Sell <info@maitag.de>
2022-07-28 06:11:09 +0200

    adding rain particle sample

---
+++ anim/rain/src/Main.hx
@@ -0,0 +1,100 @@
+package;
+
+import haxe.CallStack;
+
+import lime.app.Application;
+import lime.ui.Window;
+
+import peote.view.PeoteView;
+import peote.view.Display;
+import peote.view.Color;
+import peote.view.Texture;
+import utils.Loader;
+import lime.graphics.Image;
+
+
+class Main extends Application
+{
+	override function onWindowCreate():Void
+	{
+		switch (window.context.type)
+		{
+			case WEBGL, OPENGL, OPENGLES:
+				try startSample(window)
+				catch (_) trace(CallStack.toString(CallStack.exceptionStack()), _);
+			default: throw("Sorry, only works with OpenGL.");
+		}
+	}
+	
+	// ------------------------------------------------------------
+	// --------------- SAMPLE STARTS HERE -------------------------
+	// ------------------------------------------------------------	
+	var peoteView:PeoteView;
+	
+	public function startSample(window:Window)
+	{
+		peoteView = new PeoteView(window);
+		var display = new Display(0, 0, window.width, window.height, Color.BLACK);
+
+		peoteView.addDisplay(display);
+		
+		Loader.image ("assets/raindrop.png", true, function (image:Image) 
+		{
+			var texture = new Texture(image.width, image.height);
+			//var texture = new Texture(image.width, image.height, 1, 4, true);
+			texture.setImage(image);
+			
+			texture.tilesX = 4;
+			texture.tilesY = 1;
+			
+			Rain.init(texture, 0, 0, 392, 292, 100); // x, y, width, height, splashRangeHeight
+			// TODO: let it look isometric and set z-index in depend of where it is spawn into splashRangeHeight!
+			
+			
+			display.addProgram(Rain.program);
+			
+			Rain.duration = 2.0;
+			Rain.addDrops(100);
+			
+			// to delete
+			// haxe.Timer.delay( ()->Rain.removeDrops(99), 1000);
+		
+			// TODO:
+			// angle for the wind-direction
+			
+			
+		});
+		
+		peoteView.zoom = 2.0;
+		peoteView.start();
+	}
+	
+	
+	
+	// ------------------------------------------------------------
+	// ----------------- LIME EVENTS ------------------------------
+	// ------------------------------------------------------------	
+
+	// ----------------- MOUSE EVENTS ------------------------------
+	
+	// override function onMouseDown (x:Float, y:Float, button:lime.ui.MouseButton):Void {}
+	
+	
+	// override function onMouseMove (x:Float, y:Float):Void {}	
+	// override function onMouseUp (x:Float, y:Float, button:lime.ui.MouseButton):Void {}	
+	// override function onMouseWheel (deltaX:Float, deltaY:Float, deltaMode:lime.ui.MouseWheelMode):Void {}
+	// override function onMouseMoveRelative (x:Float, y:Float):Void {}
+
+	// ----------------- TOUCH EVENTS ------------------------------
+	// override function onTouchStart (touch:lime.ui.Touch):Void {}
+	// override function onTouchMove (touch:lime.ui.Touch):Void	{}
+	// override function onTouchEnd (touch:lime.ui.Touch):Void {}
+	
+	// ----------------- KEYBOARD EVENTS ---------------------------
+	// override function onKeyDown (keyCode:lime.ui.KeyCode, modifier:lime.ui.KeyModifier):Void {}	
+	// override function onKeyUp (keyCode:lime.ui.KeyCode, modifier:lime.ui.KeyModifier):Void {}
+
+	// -------------- other WINDOWS EVENTS ----------------------------
+	// override function onWindowResize (width:Int, height:Int):Void { trace("onWindowResize", width, height); }
+	
+}
---
+++ anim/rain/src/Rain.hx
@@ -0,0 +1,79 @@
+package;
+
+import peote.view.Element;
+import peote.view.Display;
+import peote.view.Program;
+import peote.view.Buffer;
+import peote.view.Color;
+import peote.view.Texture;
+import peote.view.utils.Util;
+
+class Rain implements Element
+{
+	// size of the raindrop
+	@sizeX public var w:Int = 8;
+	@sizeY public var h:Int = 8;
+	
+	@custom public var seed:Int = 5;
+	
+	// ranges will be changed globally by program.setFormula()
+	@custom @const var rangeLeft:Int = 0;
+	@custom @const var rangeWidth:Int = 100;
+	@custom @const var rangeTop:Int = 0;
+	@custom @const var rangeHeight:Int = 100;
+
+	@custom @const var splashOffset:Float = 0.8; // at what % of time it should start splashing
+	
+	@custom @constStart(0.0) @constEnd(1.0) @anim("repeat") var dropState:Float;
+	
+	//@posX @formula("floor(uTime/aTime0.y) * 100.0") public var x:Int; 
+	@posX @const @formula("rangeLeft + fract( sin( (floor((uTime-aTime0.x)/aTime0.y)+1.0)*seed )*100000.0) * rangeWidth") var x:Int; 
+	@posY @const @formula("rangeTop + ((dropState < splashOffset) ? dropState : splashOffset) * (rangeHeight)/splashOffset") var y:Int;
+	
+	@texTile @const @formula("(dropState < splashOffset) ? 0.0 : 1.0 + ((dropState - splashOffset)/(10.0-splashOffset*10.0)) * 30.0") var tile = 0;
+	
+	
+	static public var duration = 2.0;
+	
+	function new(w:Int = 8, h:Int = 8 ) 
+	{
+		this.w = w;
+		this.h = h;
+		this.time(Math.random()*100, duration + Math.random()*0.25);
+	}
+
+	// --------------------------------------------------------------------------
+	
+	static public var buffer:Buffer<Rain>;
+	static public var program:Program;	
+	
+	static public function init(texture:Texture, rangeLeft:Int, rangeTop:Int, rangeWidth:Int, rangeHeight:Int, splashRangeHeight:Int)
+	{	
+		buffer = new Buffer<Rain>(512, 512);
+		program = new Program(buffer);
+		
+		program.setTexture(texture, "custom");
+		program.alphaEnabled = true;
+		
+		program.setFormula("rangeLeft", Util.toFloatString(rangeLeft), false);
+		program.setFormula("rangeTop", Util.toFloatString(rangeTop), false);
+		program.setFormula("rangeWidth", Util.toFloatString(rangeWidth), false);
+		program.setFormula("rangeHeight", Util.toFloatString(rangeHeight));
+		
+		// TODO: splashRangeHeight
+		
+	}
+	
+	static public function addDrops(amount:Int) {
+		for (i in 0...amount) buffer.addElement( new Rain() );
+	}
+	
+	@:access(peote.view)
+	static public function removeDrops(amount:Int) {
+		for (i in 0...Std.int(Math.min(amount,buffer._maxElements))) {
+			buffer.removeElement( buffer._elements.get(buffer._maxElements-1) );
+		}
+	}
+		
+
+}
