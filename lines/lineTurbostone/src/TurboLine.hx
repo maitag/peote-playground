b9fe698b10fdd07d5ebe152abee560e9d86feb65
Sylvio Sell <info@maitag.de>
2023-02-04 06:15:56 +0100

    100x100

--- lines/lineTurbostone/src/TurboLine.hx
+++ lines/lineTurbostone/src/TurboLine.hx
@@ -6,8 +6,8 @@ import peote.view.Color;
 class TurboLine implements Element
 {
 	// position in pixel (relative to upper left corner of Display)
-	@posX public var x:Int;
-	@posY public var y:Int;
+	@posX public var x0:Int;
+	@posY public var y0:Int;
 	
 	// size in pixel
 	@sizeX public var w(default, null):Int;
@@ -26,13 +26,14 @@ class TurboLine implements Element
 	@pivotY public var py:Int = 0;
 */	
 	
-	public function new(x0:Int, y0:Int, x1:Int, y1:Int) {
-		set(x0, y0, x1, y1);
+	public function new(x0:Int, y0:Int, x1:Int, y1:Int, c:Color) {
+		set(x0, y0, x1, y1, c);
 	}
 
-	public function set(x0:Int, y0:Int, x1:Int, y1:Int) {
-		x = x0;
-		y = y0;
+	public function set(x0:Int, y0:Int, x1:Int, y1:Int, c:Color) {
+		this.x0 = x0;
+		this.y0 = y0;
+		this.c = c;
 		var a = x0 - x1;
 		var b = y0 - y1;	
 		w = Std.int( Math.sqrt( a * a + b * b ) );
