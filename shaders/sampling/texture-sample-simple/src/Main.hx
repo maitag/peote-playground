11b46f5ce9f06268c8016dc142837c40ced4a59c
jf <22012032+jobf@users.noreply.github.com>
2023-04-27 12:07:50 +0100

    render sprite program to texture display

--- shaders/sampling/texture-sample-simple/src/Main.hx
+++ shaders/sampling/texture-sample-simple/src/Main.hx
@@ -1,55 +1,67 @@
 package;
 
+import peote.view.Texture;
 import haxe.CallStack;
-
 import lime.app.Application;
 import lime.ui.Window;
-
 import peote.view.PeoteView;
 import peote.view.Buffer;
 import peote.view.Display;
 import peote.view.Program;
 import peote.view.Color;
 
-
-class Main extends Application
-{
-	var buffer:Buffer<Sprite>;
+class Main extends Application {
+	var spriteBuffer:Buffer<Sprite>;
 	var sprite:Sprite;
 
-
-	override function onWindowCreate():Void
-	{
-		switch (window.context.type)
-		{
+	override function onWindowCreate():Void {
+		switch (window.context.type) {
 			case WEBGL, OPENGL, OPENGLES:
-				try startSample(window)
-				catch (_) trace(CallStack.toString(CallStack.exceptionStack()), _);
-			default: throw("Sorry, only works with OpenGL.");
+				try
+					startSample(window)
+				catch (_)
+					trace(CallStack.toString(CallStack.exceptionStack()), _);
+			default:
+				throw("Sorry, only works with OpenGL.");
 		}
 	}
-	
+
 	// ------------------------------------------------------------
 	// --------------- SAMPLE STARTS HERE -------------------------
-	// ------------------------------------------------------------	
-	
-	public function startSample(window:Window)
-	{
+	// ------------------------------------------------------------
+
+	public function startSample(window:Window) {
 		var peoteView = new PeoteView(window);
+
+		// main display
 		var display = new Display(0, 0, window.width, window.height, Color.GREY4);
 		peoteView.addDisplay(display);
-		
-		buffer = new Buffer<Sprite>(4, 4, true);
-		var program = new Program(buffer);
-		display.addProgram(program);
+
+		// new display which will render to a texture
+		var texture_display = new Display(0, 0, window.width, window.height);
+		peoteView.addFramebufferDisplay(texture_display);
+
+		// the texture which the display will render to
+		var texture = new Texture(window.width, window.height);
+		texture_display.setFramebuffer(texture, peoteView);
+
+		// spriteProgram to draw on texture_display (and therefore the texture)
+		spriteBuffer = new Buffer<Sprite>(4, 4, true);
+		var spriteProgram = new Program(spriteBuffer);
+		// add sprite program to texture display
+		texture_display.addProgram(spriteProgram);
 
 		sprite = new Sprite(0, 0, 100, 100, 0xff0000ff);
-		buffer.addElement(sprite);
+		spriteBuffer.addElement(sprite);
+
+		// currently the Sprite is not visible because
+		// the texture_display is not added to PeoteView
+
 	}
-	
+
 	// ------------------------------------------------------------
 	// ----------------- LIME EVENTS ------------------------------
-	// ------------------------------------------------------------	
+	// ------------------------------------------------------------
 
 	override function onPreloadComplete():Void {
 		// access embeded assets from here
@@ -59,31 +71,27 @@ class Main extends Application
 		// for game-logic update
 	}
 
-	override function onMouseMove (x:Float, y:Float):Void {
+	override function onMouseMove(x:Float, y:Float):Void {
 		sprite.x = Std.int(x);
 		sprite.y = Std.int(y);
-		buffer.updateElement(sprite);
-	}	
+		spriteBuffer.updateElement(sprite);
+	}
 
 	// override function render(context:lime.graphics.RenderContext):Void {}
-	// override function onRenderContextLost ():Void trace(" --- WARNING: LOST RENDERCONTEXT --- ");		
-	// override function onRenderContextRestored (context:lime.graphics.RenderContext):Void trace(" --- onRenderContextRestored --- ");		
-
+	// override function onRenderContextLost ():Void trace(" --- WARNING: LOST RENDERCONTEXT --- ");
+	// override function onRenderContextRestored (context:lime.graphics.RenderContext):Void trace(" --- onRenderContextRestored --- ");
 	// ----------------- MOUSE EVENTS ------------------------------
-	// override function onMouseDown (x:Float, y:Float, button:lime.ui.MouseButton):Void {}	
-	// override function onMouseUp (x:Float, y:Float, button:lime.ui.MouseButton):Void {}	
+	// override function onMouseDown (x:Float, y:Float, button:lime.ui.MouseButton):Void {}
+	// override function onMouseUp (x:Float, y:Float, button:lime.ui.MouseButton):Void {}
 	// override function onMouseWheel (deltaX:Float, deltaY:Float, deltaMode:lime.ui.MouseWheelMode):Void {}
 	// override function onMouseMoveRelative (x:Float, y:Float):Void {}
-
 	// ----------------- TOUCH EVENTS ------------------------------
 	// override function onTouchStart (touch:lime.ui.Touch):Void {}
 	// override function onTouchMove (touch:lime.ui.Touch):Void	{}
 	// override function onTouchEnd (touch:lime.ui.Touch):Void {}
-	
 	// ----------------- KEYBOARD EVENTS ---------------------------
-	// override function onKeyDown (keyCode:lime.ui.KeyCode, modifier:lime.ui.KeyModifier):Void {}	
+	// override function onKeyDown (keyCode:lime.ui.KeyCode, modifier:lime.ui.KeyModifier):Void {}
 	// override function onKeyUp (keyCode:lime.ui.KeyCode, modifier:lime.ui.KeyModifier):Void {}
-
 	// -------------- other WINDOWS EVENTS ----------------------------
 	// override function onWindowResize (width:Int, height:Int):Void { trace("onWindowResize", width, height); }
 	// override function onWindowLeave():Void { trace("onWindowLeave"); }
@@ -99,5 +107,4 @@ class Main extends Application
 	// override function onWindowMove(x:Float, y:Float):Void { trace("onWindowMove"); }
 	// override function onWindowMinimize():Void { trace("onWindowMinimize"); }
 	// override function onWindowRestore():Void { trace("onWindowRestore"); }
-	
 }
