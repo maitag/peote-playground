2c81b15bd1a0f260e1eff2254897138182d0e44f
Sylvio Sell <info@maitag.de>
2023-06-12 21:56:32 +0200

    adding lightning folder and simple multilight-sample

---
+++ lightning/simple-multilights/src/Main.hx
@@ -0,0 +1,127 @@
+package;
+
+import haxe.CallStack;
+import lime.ui.KeyCode;
+import lime.ui.KeyModifier;
+
+import lime.app.Application;
+import lime.ui.Window;
+import lime.ui.MouseWheelMode;
+import lime.graphics.Image;
+
+import peote.view.PeoteView;
+import peote.view.Display;
+import peote.view.Color;
+import peote.view.Texture;
+import utils.Loader;
+
+
+class Main extends Application
+{
+	override function onWindowCreate():Void
+	{
+		switch (window.context.type)
+		{
+			case WEBGL, OPENGL, OPENGLES:
+				try startSample(window)
+				catch (_) trace(CallStack.toString(CallStack.exceptionStack()), _);
+			default: throw("Sorry, only works with OpenGL.");
+		}
+	}
+	
+	// ------------------------------------------------------------
+	// --------------- SAMPLE STARTS HERE -------------------------
+	// ------------------------------------------------------------	
+	var peoteView:PeoteView;
+	
+	var light:NormalLight = null; 
+	
+	public function startSample(window:Window)
+	{
+		peoteView = new PeoteView(window);
+		var display = new Display(0, 0, 512, 512);
+
+		peoteView.addDisplay(display);
+		
+		Loader.imageArray(["assets/mandelbulb_light_alpha0001.png", "assets/mandelbulb_normal0001.png"] , true, function (image:Array<Image>)
+		{
+			var lightsTexture = new Texture(image[0].width, image[0].height); // , 1, 4, false, 0, 0);
+			lightsTexture.setImage(image[0]);
+			
+			var normalTexture = new Texture(image[1].width, image[1].height); // , 1, 4, false, 0, 0);
+			normalTexture.setImage(image[1]);
+
+			
+			// TODO: render all into Texture per frame!
+			
+			// TODO: only need normalTexture here
+			NormalLight.init(display, lightsTexture, normalTexture);
+			light = new NormalLight(200, 200, 1024, 0.25, Color.RED);
+
+			// "add" (blendmode!) another light
+			for (i in 0...2048) new NormalLight(Std.random(1024), Std.random(1024), 128 + Std.random(64), Math.random()*.6, Color.random().setRed(Math.random()*0.9).setAlpha(1.0));
+			
+			// TODO: better render color-texture at first and then into a second program all the lights into "add" mode
+			
+			
+			
+			
+			
+			// add mouse events to move the light (to not run before it was instantiated):
+			window.onMouseMove.add(_onMouseMove);
+			window.onMouseWheel.add(_onMouseWheel);
+		});
+		
+		
+		//peoteView.start();
+		
+	}
+	
+	
+	
+	// ------------------------------------------------------------
+	// ----------------- LIME EVENTS ------------------------------
+	// ------------------------------------------------------------	
+	function _onMouseMove (x:Float, y:Float):Void {
+		light.x = Std.int(x);
+		light.y = Std.int(y);
+		light.update();
+	}	
+
+	var isShift = false;
+	
+	function _onMouseWheel (deltaX:Float, deltaY:Float, deltaMode:MouseWheelMode):Void {
+		if (isShift) {
+			light.size += ( (deltaY > 0) ? 1 : -1  ) * 10;
+		}
+		else {
+			light.depth += ( (deltaY > 0) ? 1 : -1  ) * 0.01;
+		}
+		trace(light.depth);
+		light.update();
+	}
+	// ----------------- MOUSE EVENTS ------------------------------
+	
+	// override function onMouseDown (x:Float, y:Float, button:lime.ui.MouseButton):Void {}	
+	// override function onMouseMove (x:Float, y:Float):Void {
+	// override function onMouseUp (x:Float, y:Float, button:lime.ui.MouseButton):Void {}	
+	// override function onMouseWheel (deltaX:Float, deltaY:Float, deltaMode:lime.ui.MouseWheelMode):Void {}
+	// override function onMouseMoveRelative (x:Float, y:Float):Void {}
+
+	// ----------------- TOUCH EVENTS ------------------------------
+	// override function onTouchStart (touch:lime.ui.Touch):Void {}
+	// override function onTouchMove (touch:lime.ui.Touch):Void	{}
+	// override function onTouchEnd (touch:lime.ui.Touch):Void {}
+	
+	// ----------------- KEYBOARD EVENTS ---------------------------
+	override function onKeyDown (keyCode:lime.ui.KeyCode, modifier:lime.ui.KeyModifier):Void {
+		if (keyCode == KeyCode.LEFT_SHIFT) isShift = true;
+	}	
+	override function onKeyUp (keyCode:lime.ui.KeyCode, modifier:lime.ui.KeyModifier):Void {
+		if (keyCode == KeyCode.LEFT_SHIFT) isShift = false;
+	}
+
+	// -------------- other WINDOWS EVENTS ----------------------------
+	// override function onWindowResize (width:Int, height:Int):Void { trace("onWindowResize", width, height); }
+	
+}
---
+++ lightning/simple-multilights/src/NormalLight.hx
@@ -0,0 +1,122 @@
+package;
+
+import peote.view.Element;
+import peote.view.Display;
+import peote.view.Program;
+import peote.view.Buffer;
+import peote.view.Color;
+import peote.view.Texture;
+import peote.view.utils.BlendFactor;
+
+class NormalLight implements Element
+{
+	// position in pixel (relative to upper left corner of Display)
+	@varying @posX public var x:Int = 50;
+	@varying @posY public var y:Int = 50;
+	
+	@varying @custom public var depth:Float = 0.25;
+	
+	// size in pixel
+	@varying @sizeX public var size:Int = 100;
+	@sizeY @const @formula("size") var h:Int;
+	
+	@pivotX @const @formula("size * 0.5") var px:Int;
+	@pivotY @const @formula("size * 0.5") var py:Int;
+	
+	@color public var color:Color;
+
+	// --------------------------------------------------------------------------
+	
+	static public var buffer:Buffer<NormalLight>;
+	static public var program:Program;
+	
+	static public function init(display:Display, lightsTexture:Texture, normalTexture:Texture)
+	{	
+		buffer = new Buffer<NormalLight>(8, 8, true);
+		program = new Program(buffer);
+		
+		// create a texture-layer named "base"
+		program.setTexture(lightsTexture, "lights", false);
+		program.setTexture(normalTexture, "normal", true);
+
+		program.injectIntoFragmentShader(
+		"			
+			vec4 normalLight( int colorTextureID, int normalTextureID, float depth )
+			{
+				//vec3 lightColor = vec3(1.0, 1.0, 1.0);
+				vec3 lightColor = vColor0.rgb;
+				vec3 ambientColor = vec3(0.0, 0.0, 0.0);
+				vec3 falloff = vec3(0.4, 3.0, 20.0); // TODO: in depend of size!
+				
+				
+				vec2 texRes = getTextureResolution(normalTextureID);
+				
+				// fetch pixel from normal-map
+				vec4 normalTextureRGBZ = getTextureColor( normalTextureID, (vPos + (vTexCoord - 0.5)*vSize)/texRes );
+				
+				// flip x normal (depends on uv-map generation variants)
+				//normalTextureRGBZ.r = 1.0 - normalTextureRGBZ.r;
+				
+				// vector to position of light
+				vec3 light = vec3(  vTexCoord - 0.5 ,  normalTextureRGBZ.a - depth );
+				
+				float D = length(light);
+				// calculate the light falloff
+				//float attenuation = 1.0 / ( falloff.x + (falloff.y * D) + (falloff.z * D * D) );
+				//float attenuation = clamp(1.0 - D * D / (0.5 * 0.5), 0.0, 1.0);
+				float attenuation = max(1.0 - D * D / (0.25), 0.0);
+				attenuation *= attenuation;
+
+				//normalize our vectors
+				vec3 N = normalize(normalTextureRGBZ.rgb * 2.0 - 1.0);
+				//vec3 N = normalTextureRGBZ.rgb * 2.0 - 1.0; // normally should be already normalized by blender!
+				vec3 L = normalize(light);
+				
+				//  dot product to determine diffuse by light and face-normal direction
+				vec3 diffuse = lightColor * max( dot(N, L), 0.0);
+	
+				// fetch pixel from color-map
+				vec4 colorTextureRGBA = getTextureColor( colorTextureID, (vPos + (vTexCoord - 0.5)*vSize)/texRes );
+				
+				// add the final attenuated light to the ambientColor
+				vec3 intensity = diffuse * attenuation;
+	
+				//return normalTextureRGBZ; // to test the normal-mapping
+				//return colorTextureRGBA; // to test the color-mapping
+				return vec4(intensity*colorTextureRGBA.rgb, colorTextureRGBA.a);
+			}			
+		");
+		
+		// instead of using normal "base" identifier to get the texture-color
+		// the "_ID" postfix is to give access to use getTextureColor() manually 
+		// from inside of the glsl blur() function to that texture-layer
+		
+		// blending to "add" multiple lights
+		program.blendEnabled = true;
+		program.blendSrc = BlendFactor.ONE;
+		program.blendDst = BlendFactor.ONE;
+				
+		program.setColorFormula( "normalLight(lights_ID, normal_ID, depth)" ); // TODO: only need UV + Z here
+		
+		
+		display.addProgram(program);
+	}
+	
+	
+	
+	public function new(x:Int = 50, y:Int = 50, size:Int = 100, depth:Float = 0.1, color:Color)
+	{
+		this.x = x;
+		this.y = y;
+		this.size = size;
+		this.depth = depth;
+		this.color = color;
+		buffer.addElement(this);
+	}
+
+	public function update()
+	{
+		buffer.updateElement(this);
+	}
+
+}
