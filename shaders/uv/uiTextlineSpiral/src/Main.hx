a1f84e995502939d939d8d3cdce6405521a7bd42
maitag <info@maitag.de>
2024-02-10 00:56:21 +0100

    new sample vor UV-mapping ;)

---
+++ shaders/uv/uiTextlineSpiral/src/Main.hx
@@ -0,0 +1,163 @@
+package;
+
+import haxe.CallStack;
+
+import lime.app.Application;
+import lime.ui.Window;
+import lime.graphics.Image;
+
+import peote.view.PeoteView;
+import peote.view.Display;
+import peote.view.Color;
+import peote.view.Texture;
+import utils.Loader;
+
+import peote.text.Font;
+
+import peote.ui.PeoteUIDisplay;
+import peote.ui.interactive.UITextLine;
+import peote.ui.style.*;
+import peote.ui.config.*;
+import peote.ui.event.*;
+
+import peote.ui.style.interfaces.FontStyle;
+
+// ------------------------------------------
+// --- using a custom FontStyle here --------
+// ------------------------------------------
+
+@packed // this is need for ttfcompile fonts (gl3font)
+@globalLineSpace // all pageLines using the same page.lineSpace (gap to next line into page)
+@:structInit
+class MyFontStyle implements FontStyle
+{
+	public var color:Color = Color.GREEN;
+	//public var color:Color = Color.GREEN.setAlpha(0.5);
+	public var width:Float = 38; // (<- is it still fixed to get from font-defaults if this is MISSING ?)
+	public var height:Float = 36;
+	@global public var weight = 0.5; //0.49 <- more thick (only for ttfcompiled fonts)
+}
+
+class Main extends Application
+{
+	override function onWindowCreate():Void
+	{
+		switch (window.context.type)
+		{
+			case WEBGL, OPENGL, OPENGLES:
+				try startSample()
+				catch (_) trace(CallStack.toString(CallStack.exceptionStack()), _);
+			default: throw("Sorry, only works with OpenGL.");
+		}
+	}
+	
+	// ------------------------------------------------------------
+	// --------------- SAMPLE STARTS HERE -------------------------
+	// ------------------------------------------------------------	
+	var peoteView:PeoteView;
+	
+	var uvMap:UVmap; 
+	
+	public function startSample()
+	{
+		Loader.image("assets/spiral8bpc.png", true, function(image:Image) {		
+			new Font<MyFontStyle>("assets/hackfont/config.json").load( function(font:Font<MyFontStyle>) {
+				allLoaded(image, font);
+			});
+		});
+	}
+	
+	public function allLoaded(image:Image, font:Font<MyFontStyle>)
+	{
+		peoteView = new PeoteView(window);
+		var display = new Display(0, 0, window.width, window.height, Color.GREY1);
+
+		peoteView.addDisplay(display);
+		
+		var uvTexture = new Texture(image.width, image.height, 1, 4, false, 0, 0);
+		uvTexture.setImage(image);
+			
+		var fbTexture = new Texture(4096, 40, 1, 4, false, 1, 0);
+		
+		// set up Display for framebuffer
+		var fontStyle = new MyFontStyle();
+		
+		var boxStyle = new BoxStyle(Color.BLACK);
+		
+		var uiDisplay = new PeoteUIDisplay(0, 0, 4096, 40);
+		peoteView.setFramebuffer(uiDisplay, fbTexture);
+
+		peoteView.addFramebufferDisplay(uiDisplay);
+		// peoteView.addDisplay(uiDisplay);
+
+		// ------------------- input TextLine ---------------------------
+		
+		var fontStyleInput = new MyFontStyle();
+		fontStyleInput.color = Color.GREY5;
+		//fontStyleInput.height = 30;
+		//fontStyleInput.width = 20;
+		
+		var textConfig:TextConfig = {
+			backgroundStyle:boxStyle,
+			selectionStyle:BoxStyle.createById(1, Color.GREY3), // new ID for new Layer
+			cursorStyle:BoxStyle.createById(2, Color.RED),       // new ID for new Layer
+			//hAlign:HAlign.RIGHT,
+			undoBufferSize: 30
+		}
+		
+		var inputLine = new UITextLine<MyFontStyle>(2, 2, 0, 0, "Haxe", font, fontStyleInput, textConfig);
+		uiDisplay.add(inputLine);
+		inputLine.setInputFocus();
+
+		PeoteUIDisplay.registerEvents(window);
+
+			
+		// -------------------------------------------------
+
+		UVmap.init(display, uvTexture, fbTexture);
+		uvMap = new UVmap(0, 0, 600, 600);
+		
+		uvMap.timeOffset(0.0, 1.0);
+		UVmap.buffer.updateElement(uvMap);
+		
+		peoteView.start();
+		
+		
+	}
+	
+	
+	
+	// ------------------------------------------------------------
+	// ----------------- LIME EVENTS ------------------------------
+	// ------------------------------------------------------------	
+
+	// ----------------- MOUSE EVENTS ------------------------------
+	
+	// override function onMouseDown (x:Float, y:Float, button:lime.ui.MouseButton):Void {}	
+	// override function onMouseMove (x:Float, y:Float):Void {}	
+	// override function onMouseUp (x:Float, y:Float, button:lime.ui.MouseButton):Void {}
+
+	var speed:Float = 1.0;
+	override function onMouseWheel (deltaX:Float, deltaY:Float, deltaMode:lime.ui.MouseWheelMode):Void {
+		trace(deltaY, deltaY);
+		if (deltaY > 0 &&speed < 3.0) speed += 0.01;
+		else if (speed > 0.05) speed -= 0.01;
+			
+		uvMap.timeOffset(1.0-speed, speed);
+		UVmap.buffer.updateElement(uvMap);
+	}
+	// override function onMouseMoveRelative (x:Float, y:Float):Void {}
+
+	// ----------------- TOUCH EVENTS ------------------------------
+	// override function onTouchStart (touch:lime.ui.Touch):Void {}
+	// override function onTouchMove (touch:lime.ui.Touch):Void	{}
+	// override function onTouchEnd (touch:lime.ui.Touch):Void {}
+	
+	// ----------------- KEYBOARD EVENTS ---------------------------
+	// override function onKeyDown (keyCode:lime.ui.KeyCode, modifier:lime.ui.KeyModifier):Void {}	
+	// override function onKeyUp (keyCode:lime.ui.KeyCode, modifier:lime.ui.KeyModifier):Void {}
+
+	// -------------- other WINDOWS EVENTS ----------------------------
+	// override function onWindowResize (width:Int, height:Int):Void { trace("onWindowResize", width, height); }
+	
+}
---
+++ shaders/uv/uiTextlineSpiral/src/UVmap.hx
@@ -0,0 +1,96 @@
+package;
+
+import peote.view.Element;
+import peote.view.Display;
+import peote.view.Program;
+import peote.view.Buffer;
+import peote.view.Color;
+import peote.view.Texture;
+
+class UVmap implements Element
+{
+	// position in pixel (relative to upper left corner of Display)
+	@posX public var x:Int;
+	@posY public var y:Int;
+	
+	// size in pixel
+	@sizeX public var w:Int;
+	@sizeY public var h:Int;
+	
+	//let the texture shift/resize inside slot/texCoords/tile area of Element
+	//@texPosX("image")public var txOffset:Int = 3000;
+	@texPosX("image") @constStart(3050) @constEnd(3000) @anim("offset", "constant") public var txOffset:Int;
+	@texPosY("image") public var tyOffset:Int = 0;
+	//@texSizeX("image") public var twOffset:Int = 5000;
+	@texSizeY("image") public var thOffset:Int = 24;
+	
+	var OPTIONS = { texRepeatX:true, texRepeatY:false, blend:true };
+	
+	// TODO:
+	//var DEFAULT_FRAGMENT_SHADER = "";	
+	
+	//var DEFAULT_COLOR_FORMULA = "'uvmapping(${UVmap.TEXTURE_uvmap}.rgb, ${UVmap.TEXTURE_ID_image})'";
+	
+	//var DEFAULT_FORMULA_VARS = ["uvmap"  => 0x00ff00ff];
+	//var DEFAULT_FORMULA_VARS = ["image"  => 0xff0000ff];
+	// --------------------------------------------------------------------------
+	
+	static public var buffer:Buffer<UVmap>;
+	static public var program:Program;
+	
+	static public function init(display:Display, uvTexture:Texture, imageTexture:Texture)
+	{	
+		buffer = new Buffer<UVmap>(1, 1, true);
+		program = new Program(buffer);
+		
+		// create named texture-layer
+		program.setTexture(uvTexture, "uvmap"); // holds the upmap
+		program.setTexture(imageTexture, "image", true); // holds the image that need to transformate via uvmap
+
+		program.injectIntoFragmentShader(
+		"				
+			vec4 uvmapping( vec3 uvRGB, int imgTexID )
+			{		
+				
+				// using red + blue colorchannels for horizontal mapping
+				//float highByte = uvRGB.r * (256.0 / 257.0);
+				//float lowByte  = uvRGB.b * (1.0 / 257.0);				
+				//float uCoord = highByte + lowByte;
+				
+				float uCoord =  (uvRGB.r * 256.0 + uvRGB.b ) / 257.0;
+				
+				// using green colorchannel for vertically mapping
+				float vCoord = uvRGB.g;
+
+				//vec2 imgTexRes = getTextureResolution(imgTexID);
+				
+				return getTextureColor( imgTexID, vec2( uCoord, vCoord ) );
+				//return getTextureColor( imgTexID, vec2( clamp(uCoord*1.0, 0.0, 1.0), clamp(vCoord*1.0, 0.0, 1.0) ) );
+				
+				//return vec4(uvRGB, 1.0); // to shows the pure uvmap uncomment here!
+			}			
+		");
+		
+		// instead of using normal "image" identifier to get the texture-color,
+		// the "_ID" postfix is to give access to use getTextureColor() manually 
+		// from inside of the glsl uvmapping() function for that texture-layer
+		
+		program.setColorFormula( "uvmapping(uvmap.rgb, image_ID)" );
+				
+		//program.setFragmentFloatPrecision("high", true);
+		
+		display.addProgram(program);
+	}
+	
+	
+	
+	public function new(x:Int = 0, y:Int = 0, w:Int = 100, h:Int = 100) 
+	{
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+		buffer.addElement(this);
+	}
+
+}
