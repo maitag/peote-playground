3e50a0e145f1e3120252caa154a7c2248e7a75c8
maitag <info@maitag.de>
2024-03-16 00:33:58 +0100

    new sample for peote-view and using the vision haxelib

---
+++ vision/src/Main.hx
@@ -0,0 +1,94 @@
+package;
+
+import haxe.CallStack;
+
+import lime.app.Application;
+import lime.ui.Window;
+
+import peote.view.*;
+
+import vision.ds.Image;
+import vision.ds.Color as VisionColor;
+
+
+
+class Elem implements Element
+{
+	@posX public var x:Int;
+	@posY public var y:Int;
+	
+	@sizeX public var w:Int;
+	@sizeY public var h:Int;
+	
+	public function new(x:Int=0, y:Int=0, w:Int=100, h:Int=100)
+	{
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+	}
+}
+
+
+
+class Main extends Application
+{	
+	override function onWindowCreate():Void
+	{
+		switch (window.context.type)
+		{
+			case WEBGL, OPENGL, OPENGLES:
+				try startSample(window)
+				catch (_) trace(CallStack.toString(CallStack.exceptionStack()), _);
+			default: throw("Sorry, only works with OpenGL.");
+		}
+	}
+
+	public function startSample(window:Window)
+	{	
+
+		// create a new vision-image with a gold background
+		var image = new Image(512, 512, VisionColor.GOLD);
+
+		
+		// draw something inside
+		for (y in 0...128) {
+			for (x in 0...256) {
+				image.setPixel(x, y, VisionColor.BLUE);
+			}	
+		}
+		
+
+		// create new peote-view texture
+		var texture = new Texture(512, 512);
+
+		// put vision-image data into peote-view texture
+		texture.setData(image);
+		
+
+
+		// -------------------------------------------------------------
+
+
+		// set up a view, display, program and a buffer with one Element
+		var peoteView = new PeoteView(window);
+		var display   = new Display(10, 10, 512, 512, Color.GREY3);
+
+		peoteView.addDisplay(display);  // display to view
+		
+		var buffer  = new Buffer<Elem>(10);
+		var program = new Program(buffer);
+		
+		var element = new Elem(0, 0, 512, 512);
+		buffer.addElement(element);
+		
+		// add texture to program
+		program.setTexture(texture, "custom");
+		//program.discardAtAlpha(0.1);
+		//program.alphaEnabled = true;
+			
+		display.addProgram(program);  // programm to display
+	}
+	
+
+}
