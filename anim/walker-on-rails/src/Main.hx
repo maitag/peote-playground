14c9ad67ba07f2e897292891bdfa445130ff0115
maitag <info@maitag.de>
2024-04-16 03:14:26 +0200

    another walker environment to make it also choose the direction of WALK :)

---
+++ anim/walker-on-rails/src/Main.hx
@@ -0,0 +1,126 @@
+package;
+
+import haxe.CallStack;
+
+import lime.app.Application;
+import lime.ui.Window;
+import lime.graphics.Image;
+
+import peote.view.*;
+import peote.view.TextureData;
+import utils.Loader;
+
+class Main extends Application
+{
+	override function onWindowCreate():Void
+	{
+		switch (window.context.type)
+		{
+			case WEBGL, OPENGL, OPENGLES:
+				try startSample(window)
+				catch (_) trace(CallStack.toString(CallStack.exceptionStack()), _);
+			default: throw("Sorry, only works with OpenGL.");
+		}
+	}
+	
+	// ------------------------------------------------------------
+	// --------------- SAMPLE STARTS HERE -------------------------
+	// ------------------------------------------------------------	
+	
+	public function startSample(window:Window)
+	{
+		var peoteView = new PeoteView(window);
+
+		var buffer = new Buffer<Walker>(4, 4, true);
+		var display = new Display(0, 0, window.width, window.height, Color.GREY1);
+		var program = new Program(buffer);
+
+		peoteView.addDisplay(display);
+		display.addProgram(program);
+		
+		Loader.bytes("assets/walkGreyAlpha.png", true, function(bytes:haxe.io.Bytes)
+		{
+			var texture = new Texture(1024, 384, {format:TextureFormat.LUMINANCE_ALPHA});
+			texture.setData( TextureData.fromFormatPNG(bytes) );
+
+			texture.tilesX = 8;
+			texture.tilesY = 3;
+	
+			program.addTexture(texture, "custom");
+
+			// here some -> R A I L S <- (^_^)
+			var y:Int = 0;
+			var s:Int = 7;
+			while (y < 768) {
+				var walker = new Walker(y, s);
+
+				buffer.addElement(walker);
+
+				walker.goLeftOrRight(window.width, Math.random() * 2.3 + 1.4);
+				
+				
+				// don't forget to update after changing for tile-anim!
+				buffer.updateElement(walker);
+
+
+				y+=s+=4;
+			}
+			
+			// after this the "peote time" counts up !
+			peoteView.start();
+		});
+	
+	}
+	
+
+
+
+	// ------------------------------------------------------------
+	// ----------------- LIME EVENTS ------------------------------
+	// ------------------------------------------------------------	
+
+	// override function onPreloadComplete():Void {
+		// access embeded assets from here
+	// }
+
+	// override function update(deltaTime:Int):Void {
+		// for game-logic update
+	// }
+
+	// override function render(context:lime.graphics.RenderContext):Void {}
+	// override function onRenderContextLost ():Void trace(" --- WARNING: LOST RENDERCONTEXT --- ");		
+	// override function onRenderContextRestored (context:lime.graphics.RenderContext):Void trace(" --- onRenderContextRestored --- ");		
+
+	// ----------------- MOUSE EVENTS ------------------------------
+	// override function onMouseMove (x:Float, y:Float):Void {}	
+	// override function onMouseDown (x:Float, y:Float, button:lime.ui.MouseButton):Void {}	
+	// override function onMouseUp (x:Float, y:Float, button:lime.ui.MouseButton):Void {}	
+	// override function onMouseWheel (deltaX:Float, deltaY:Float, deltaMode:lime.ui.MouseWheelMode):Void {}
+	// override function onMouseMoveRelative (x:Float, y:Float):Void {}
+
+	// ----------------- TOUCH EVENTS ------------------------------
+	// override function onTouchStart (touch:lime.ui.Touch):Void {}
+	// override function onTouchMove (touch:lime.ui.Touch):Void	{}
+	// override function onTouchEnd (touch:lime.ui.Touch):Void {}
+	
+	// ----------------- KEYBOARD EVENTS ---------------------------
+	// override function onKeyDown (keyCode:lime.ui.KeyCode, modifier:lime.ui.KeyModifier):Void {}	
+	// override function onKeyUp (keyCode:lime.ui.KeyCode, modifier:lime.ui.KeyModifier):Void {}
+
+	// -------------- other WINDOWS EVENTS ----------------------------
+	// override function onWindowResize (width:Int, height:Int):Void { trace("onWindowResize", width, height); }
+	// override function onWindowLeave():Void { trace("onWindowLeave"); }
+	// override function onWindowActivate():Void { trace("onWindowActivate"); }
+	// override function onWindowClose():Void { trace("onWindowClose"); }
+	// override function onWindowDeactivate():Void { trace("onWindowDeactivate"); }
+	// override function onWindowDropFile(file:String):Void { trace("onWindowDropFile"); }
+	// override function onWindowEnter():Void { trace("onWindowEnter"); }
+	// override function onWindowExpose():Void { trace("onWindowExpose"); }
+	// override function onWindowFocusIn():Void { trace("onWindowFocusIn"); }
+	// override function onWindowFocusOut():Void { trace("onWindowFocusOut"); }
+	// override function onWindowFullscreen():Void { trace("onWindowFullscreen"); }
+	// override function onWindowMove(x:Float, y:Float):Void { trace("onWindowMove"); }
+	// override function onWindowMinimize():Void { trace("onWindowMinimize"); }
+	// override function onWindowRestore():Void { trace("onWindowRestore"); }
+	
+}
---
+++ anim/walker-on-rails/src/Walker.hx
@@ -0,0 +1,56 @@
+package;
+
+import Math;
+import peote.view.Element;
+
+class Walker implements Element
+{
+	// only x position is ANIMATED here 
+	@posX @anim("X", "repeat")
+	public var x:Int;
+
+	@posY public var y:Int;
+	
+	// custom "size" attribute
+	@custom public var size:Int;
+
+
+	// ---- calculated by formula inside of glsl ----
+
+	@sizeX @const @formula("size * ((xStart < xEnd) ? -1.0 : 1.0)")
+	var w:Int;
+	
+	// height allways same as width (because it is also `quad` inside of texture-data!)
+	@sizeY @const @formula("size") // but allways keep it non-mirrored here!
+	var h:Int;
+
+	// pivot point X need to change if size is negative (tile is x-mirrored)
+	@pivotX	@const @formula("(xStart < xEnd) ? -size : 0.0") 
+	var px:Int;
+
+	// texture tile number
+	@texTile @const @formula("floor(mod(  ((xStart < xEnd) ? 1.0 : -1.0) *  x * 0.15 * 128.0/size , 23.0))")
+	var tile:Int;
+	
+	var OPTIONS = { blend:true };
+
+	public function new(y:Int, size:Int) {
+		this.y = y;
+		this.size = size;
+	}
+
+	public function goLeft(way:Int, duration:Float) {
+		animX(way, -size);
+		timeX(0.0, duration);
+	}
+
+	public function goRight(way:Int, duration:Float) {
+		animX(-size, way);
+		timeX(0.0, duration);
+	}
+
+	public function goLeftOrRight(way:Int, duration:Float) {
+		if (Math.random() < 0.5) goLeft(way, duration) else goRight(way, duration);
+	}
+
+}
