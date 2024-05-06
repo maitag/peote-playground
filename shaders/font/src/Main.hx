b7e30cb90320f8f0d5efbe87bc291feee2217138
maitag <info@maitag.de>
2024-05-06 23:36:08 +0200

    new shader sample for glsl-glyphes for 8x12 bitmapfont

---
+++ shaders/font/src/Main.hx
@@ -0,0 +1,80 @@
+package;
+
+import haxe.CallStack;
+
+import lime.app.Application;
+import lime.ui.Window;
+import lime.graphics.Image;
+
+import peote.view.*;
+import utils.Loader;
+
+
+class Main extends Application
+{
+	override function onWindowCreate():Void
+	{
+		switch (window.context.type)
+		{
+			case WEBGL, OPENGL, OPENGLES:
+				try startSample(window)
+				catch (_) trace(CallStack.toString(CallStack.exceptionStack()), _);
+			default: throw("Sorry, only works with OpenGL.");
+		}
+	}
+	
+	// ------------------------------------------------------------
+	// --------------- SAMPLE STARTS HERE -------------------------
+	// ------------------------------------------------------------		
+	
+	public function startSample(window:Window)
+	{
+		var peoteView = new PeoteView(window);
+		var display = new Display(0, 0, window.width, window.height, Color.GREY1);
+
+		peoteView.addDisplay(display);
+
+		Glyph.init(display);
+
+		var cursorX = 0;
+		var scale = 10;
+
+		var w = 8 * scale;
+		var h = 12 * scale;
+
+		new Glyph(cursorX     , 0, w, h, "H".charCodeAt(0)-32, Color.GOLD, Color.BLUE);
+		new Glyph(cursorX += w, 0, w, h, "E".charCodeAt(0)-32, Color.GOLD, Color.BLUE);
+		new Glyph(cursorX += w, 0, w, h, "L".charCodeAt(0)-32, Color.GOLD, Color.BLUE);
+		new Glyph(cursorX += w, 0, w, h, "L".charCodeAt(0)-32, Color.GOLD, Color.BLUE);
+		new Glyph(cursorX += w, 0, w, h, "O".charCodeAt(0)-32, Color.GOLD, Color.BLUE);
+		new Glyph(cursorX += w, 0, w, h, " ".charCodeAt(0)-32, Color.GOLD, Color.BLUE);
+		new Glyph(cursorX += w, 0, w, h, "?".charCodeAt(0)-32, Color.GOLD, Color.BLUE);
+		
+	}
+	
+
+
+	// ------------------------------------------------------------
+	// ----------------- LIME EVENTS ------------------------------
+	// ------------------------------------------------------------	
+
+	// ----------------- MOUSE EVENTS ------------------------------
+
+	//override function onMouseDown (x:Float, y:Float, button:lime.ui.MouseButton):Void {}
+	// override function onMouseUp (x:Float, y:Float, button:lime.ui.MouseButton):Void {}	
+	// override function onMouseWheel (deltaX:Float, deltaY:Float, deltaMode:lime.ui.MouseWheelMode):Void {}
+	// override function onMouseMoveRelative (x:Float, y:Float):Void {}
+
+	// ----------------- TOUCH EVENTS ------------------------------
+	// override function onTouchStart (touch:lime.ui.Touch):Void {}
+	// override function onTouchMove (touch:lime.ui.Touch):Void	{}
+	// override function onTouchEnd (touch:lime.ui.Touch):Void {}
+	
+	// ----------------- KEYBOARD EVENTS ---------------------------
+	// override function onKeyDown (keyCode:lime.ui.KeyCode, modifier:lime.ui.KeyModifier):Void {}	
+	// override function onKeyUp (keyCode:lime.ui.KeyCode, modifier:lime.ui.KeyModifier):Void {}
+
+	// -------------- other WINDOWS EVENTS ----------------------------
+	// override function onWindowResize (width:Int, height:Int):Void { trace("onWindowResize", width, height); }
+	
+}
