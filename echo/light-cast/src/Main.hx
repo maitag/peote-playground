014a76e4314c54aa884dfde527a804534505116b
jf <22012032+jobf@users.noreply.github.com>
2024-10-09 21:27:25 +0100

    add comments and some slight cleaning

--- echo/light-cast/src/Main.hx
+++ echo/light-cast/src/Main.hx
@@ -1,20 +1,16 @@
 package;
 
-import lime.graphics.Image;
 import utils.Loader;
+import peote.view.*;
+import echo.Line;
+import echo.Body;
+import echo.Echo;
+import echo.World;
+import echo.math.Vector2;
+import lime.graphics.Image;
 import lime.ui.KeyCode;
 import lime.ui.KeyModifier;
 import lime.ui.MouseWheelMode;
-import echo.Line;
-import echo.Body;
-import echo.math.Vector2;
-import peote.view.Buffer;
-import peote.view.Program;
-import peote.view.Texture;
-import peote.view.Display;
-import echo.Echo;
-import echo.World;
-import peote.view.PeoteView;
 import lime.app.Application;
 import haxe.CallStack;
 
@@ -54,56 +50,57 @@ class Main extends Application {
 	}
 
 	function startSample() {
-		var widthWorld = window.width;
-		var heightWorld = window.height;
 
+		mouse = new Vector2(0, 0);
+		var widthWindow = window.width;
+		var heightWindow = window.height;
+		
+		var peoteView = new PeoteView(window);
+
+		// for displaying elements behind the light/shadows
+		var background_display = new Display(0, 0, widthWindow, heightWindow);
+		peoteView.addDisplay(background_display);
+
+		// rays are rendered to texture
+		var fbo_rays = {
+			display: new Display(0, 0, widthWindow, heightWindow),
+			texture: new Texture(widthWindow, heightWindow)
+		}
+		peoteView.setFramebuffer(fbo_rays.display, fbo_rays.texture);
+		peoteView.renderToTexture(fbo_rays.display);
+		peoteView.addFramebufferDisplay(fbo_rays.display);
+		
+		// shapes are rendered to 
+		var shapes_display = new Display(0, 0, widthWindow, heightWindow);
+		peoteView.addDisplay(shapes_display);
+		
+		// add the rays display to be visible, but hide it (for debugging only)
+		peoteView.addDisplay(fbo_rays.display);
+		fbo_rays.display.hide();
+		shapes_display.hide(); // also hide shapes for now
+
+		// toggle ray/shape elements visibility on/off with mouse click
+		window.onMouseDown.add((x, y, button) -> {
+			if(fbo_rays.display.isVisible){
+				fbo_rays.display.hide();
+				shapes_display.hide();
+			}
+			else{
+				fbo_rays.display.show();
+				shapes_display.show();
+			}
+		});
+		
+		// set up the world for echo physics
 		world = Echo.start({
-			width: widthWorld,
-			height: heightWorld,
+			width: widthWindow,
+			height: heightWindow,
 			x: 0,
 			y: 0,
 		});
 
-		var peoteView = new PeoteView(window);
+		// set up rectangles to block the rays
 
-		var background_display = new Display(0, 0, widthWorld, heightWorld);
-		peoteView.addDisplay(background_display);
-
-		var fbo_rays = {
-			display: new Display(0, 0, widthWorld, heightWorld),
-			texture: new Texture(widthWorld, heightWorld)
-		}
-
-		peoteView.addDisplay(fbo_rays.display);
-		peoteView.setFramebuffer(fbo_rays.display, fbo_rays.texture);
-		peoteView.renderToTexture(fbo_rays.display);
-		peoteView.addFramebufferDisplay(fbo_rays.display);
-
-		var fbo_shapes = {
-			display: new Display(0, 0, widthWorld, heightWorld),
-			texture: new Texture(widthWorld, heightWorld)
-		}
-
-		peoteView.addDisplay(fbo_shapes.display);
-		peoteView.setFramebuffer(fbo_shapes.display, fbo_shapes.texture);
-		peoteView.renderToTexture(fbo_shapes.display);
-		peoteView.addFramebufferDisplay(fbo_shapes.display);
-
-		var toggle_shapes = ()-> {
-			if(fbo_shapes.display.isVisible){
-				fbo_shapes.display.hide();
-				fbo_rays.display.hide();
-			}
-			else{
-				fbo_shapes.display.show();
-				fbo_rays.display.show();
-			}
-		}
-
-		toggle_shapes();
-		
-		window.onMouseDown.add((x, y, button) -> toggle_shapes());
-		
 		var rectangles = [
 			{ x: 60, y: 60, w: 30, h: 30, r: 0.0 },
 			{ x: 100, y: 200, w: 75, h: 30, r: 12.0 },
@@ -112,8 +109,8 @@ class Main extends Application {
 
 		rectangle_buffer = new Buffer<Shape>(rectangles.length);
 		var rectangle_program = new Program(rectangle_buffer);
-		fbo_shapes.display.addProgram(rectangle_program);
-
+		shapes_display.addProgram(rectangle_program);
+		
 		for (rect in rectangles) {
 			var body = world.make({
 				shape: {
@@ -124,7 +121,6 @@ class Main extends Application {
 				x: rect.x,
 				y: rect.y,
 				rotational_velocity: rect.r,
-				// kinematic: true
 			});
 
 			bodies.push(body);
@@ -139,14 +135,16 @@ class Main extends Application {
 			body.on_rotate = a -> element.angle = a;
 		}
 
+		// set up circles to block the rays		
 
 		var circles = [
 			{ x: 500, y: 560,	r: 150 },
 			{ x: 660, y: 150, r: 90 },
 		];
+
 		circle_buffer = new Buffer<Shape>(circles.length);
 		var circle_program = new Program(circle_buffer);
-		fbo_shapes.display.addProgram(circle_program);
+		shapes_display.addProgram(circle_program);
 		
 		circle_program.injectIntoFragmentShader('
 		vec4 circle(vec4 color, vec2 pos)
@@ -177,8 +175,10 @@ class Main extends Application {
 			}
 		}
 
+		// set up the rays, total rays visible is controlled by mouse wheel, thickesn decrease/increase with keyboard j/k
+		
 		ray_count = 360;
-		ray_length = Std.int(widthWorld * 1.5);
+		ray_length = Std.int(widthWindow * 1.5);
 		ray_count_maximum = ray_count * 4;
 		ray_thickness = 12;
 
@@ -186,9 +186,9 @@ class Main extends Application {
 		var ray_program = new Program(ray_buffer);
 		fbo_rays.display.addProgram(ray_program);
 
-		mouse = new Vector2(0, 0);
 
-		var display = new Display(0, 0, widthWorld, heightWorld);
+		// this will display the 'darkness' (shadow) with rays lightening some areas
+		var display = new Display(0, 0, widthWindow, heightWindow);
 		peoteView.addDisplay(display);
 		lighting_buffer = new Buffer<Lighting>(1);
 		var program = new Program(lighting_buffer);
@@ -213,17 +213,19 @@ class Main extends Application {
 		program.setColorFormula('compose(darkness, rays_ID, ray_origin_x, ray_origin_y)');
 
 		display.addProgram(program);
-		view = lighting_buffer.addElement(new Lighting(0, 0, widthWorld, heightWorld));
+		view = lighting_buffer.addElement(new Lighting(0, 0, widthWindow, heightWindow));
+		// give the darkness some color and change alpha to not be fully opaque
 		view.darkness.bF = 0.02;
 		view.darkness.gF = 0.02;
 		view.darkness.aF = 0.9;
 
+		// load background image and let update is_ready!
 		Loader.image("assets/test0.png", (image:Image) -> {
 			var buffer_ = new Buffer<Shape>(1);
 			var program_ = new Program(buffer_);
 			program_.setTexture(Texture.fromData(image), "background");
 			background_display.addProgram(program_);
-			var bg = new Shape(0, 0, widthWorld, heightWorld, 0xffffffFF);
+			var bg = new Shape(0, 0, widthWindow, heightWindow, 0xffffffFF);
 			bg.pivot_x = 0.0;
 			bg.pivot_y = 0.0;
 
@@ -232,11 +234,13 @@ class Main extends Application {
 		});
 	}
 
+	// for tracking mouse position
 	override function onMouseMove(x:Float, y:Float) {
 		mouse.x = x;
 		mouse.y = y;
 	}
 
+	// for changing total rays
 	override function onMouseWheel(deltaX:Float, deltaY:Float, deltaMode:MouseWheelMode) {
 		var change = deltaY > 0 ? 1 : -1;
 		var next_ray_count = ray_count + change * 10;
@@ -245,6 +249,7 @@ class Main extends Application {
 		}
 	}
 
+	// for changing size of rays
 	override function onKeyDown(keyCode:KeyCode, modifier:KeyModifier) {
 		var change = switch keyCode {
 			case J: -1;
@@ -261,27 +266,35 @@ class Main extends Application {
 		if (!is_ready)
 			return;
 
-		rectangle_buffer.update();
-		lighting_buffer.update();
-		circle_buffer.update();
-
+		// step physics simulation
 		world.step(deltaTime / 1000);
 		view.ray_origin_x = mouse.x;
 		view.ray_origin_y = mouse.y;
 
-		var line = Line.get();
+		// clear old rays (fres ones added below)
 		ray_buffer.clear();
+
+		var line = Line.get();
+
 		for (n in 0...ray_count) {
 			line.set_from_vector(mouse, 360 * (n / ray_count), ray_length);
 			var intersection = line.linecast(bodies, world);
+			// if there is intersection then the ray hit an obstacle (ray will be shorter)
 			if (intersection != null && intersection.closest != null) {
 				var data = intersection.closest;
 				ray_buffer.addElement(new Ray(data.line.start.x, data.line.start.y, data.hit.x, data.hit.y, ray_thickness, 0xffffffFF));
-			} else {
+			} 
+			// else ray can be full length
+			else {
 				ray_buffer.addElement(new Ray(line.start.x, line.start.y, line.end.x, line.end.y, ray_thickness, 0xffffffFF));
 			}
 		}
 
 		line.put();
+
+		// update the element buffers
+		rectangle_buffer.update();
+		lighting_buffer.update();
+		circle_buffer.update();
 	}
 }
