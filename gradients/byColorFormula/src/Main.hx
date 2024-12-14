4273473fa6782851b5258ca6197202938db28bd6
maitag <info@maitag.de>
2024-12-14 23:56:46 +0100

    adding sample to use simple vTexCoord inside Colorformula to make gradients

---
+++ gradients/byColorFormula/src/Main.hx
@@ -0,0 +1,102 @@
+package;
+
+import haxe.CallStack;
+
+import lime.app.Application;
+import lime.ui.Window;
+
+import peote.view.PeoteView;
+import peote.view.Buffer;
+import peote.view.Display;
+import peote.view.Program;
+import peote.view.Color;
+
+import peote.view.element.Elem;
+
+
+class Main extends Application
+{
+	override function onWindowCreate():Void
+	{
+		switch (window.context.type)
+		{
+			case WEBGL, OPENGL, OPENGLES:
+				try startSample(window)
+				catch (_) trace(CallStack.toString(CallStack.exceptionStack()), _);
+			default: throw("Sorry, only works with OpenGL.");
+		}
+	}
+	
+	// ------------------------------------------------------------
+	// --------------- SAMPLE STARTS HERE -------------------------
+	// ------------------------------------------------------------	
+	
+	public function startSample(window:Window)
+	{
+		var peoteView = new PeoteView(window);
+
+		var buffer = new Buffer<Elem>(4, 4, true);
+		var display = new Display(10, 10, window.width - 20, window.height - 20, Color.BLUE);
+		var program = new Program(buffer);
+
+		peoteView.addDisplay(display);
+		display.addProgram(program);
+
+
+		// simple use of color formula for the program
+		program.setColorFormula("vec4(vTexCoord.x, vTexCoord.y, 0.0, 1.0)");
+
+
+		buffer.addElement(new Elem(10,10, 300, 300, 0.0, 0, 0, 0, Color.RED));
+		buffer.addElement(new Elem(400,10, 200, 200, 0.0, 0, 0, 0, Color.BLUE));
+	}
+	
+	// ------------------------------------------------------------
+	// ----------------- LIME EVENTS ------------------------------
+	// ------------------------------------------------------------	
+
+	override function onPreloadComplete():Void {
+		// access embeded assets from here
+	}
+
+	override function update(deltaTime:Int):Void {
+		// for game-logic update
+	}
+
+	// override function render(context:lime.graphics.RenderContext):Void {}
+	// override function onRenderContextLost ():Void trace(" --- WARNING: LOST RENDERCONTEXT --- ");		
+	// override function onRenderContextRestored (context:lime.graphics.RenderContext):Void trace(" --- onRenderContextRestored --- ");		
+
+	// ----------------- MOUSE EVENTS ------------------------------
+	// override function onMouseMove (x:Float, y:Float):Void {}	
+	// override function onMouseDown (x:Float, y:Float, button:lime.ui.MouseButton):Void {}	
+	// override function onMouseUp (x:Float, y:Float, button:lime.ui.MouseButton):Void {}	
+	// override function onMouseWheel (deltaX:Float, deltaY:Float, deltaMode:lime.ui.MouseWheelMode):Void {}
+	// override function onMouseMoveRelative (x:Float, y:Float):Void {}
+
+	// ----------------- TOUCH EVENTS ------------------------------
+	// override function onTouchStart (touch:lime.ui.Touch):Void {}
+	// override function onTouchMove (touch:lime.ui.Touch):Void	{}
+	// override function onTouchEnd (touch:lime.ui.Touch):Void {}
+	
+	// ----------------- KEYBOARD EVENTS ---------------------------
+	// override function onKeyDown (keyCode:lime.ui.KeyCode, modifier:lime.ui.KeyModifier):Void {}	
+	// override function onKeyUp (keyCode:lime.ui.KeyCode, modifier:lime.ui.KeyModifier):Void {}
+
+	// -------------- other WINDOWS EVENTS ----------------------------
+	// override function onWindowResize (width:Int, height:Int):Void { trace("onWindowResize", width, height); }
+	// override function onWindowLeave():Void { trace("onWindowLeave"); }
+	// override function onWindowActivate():Void { trace("onWindowActivate"); }
+	// override function onWindowClose():Void { trace("onWindowClose"); }
+	// override function onWindowDeactivate():Void { trace("onWindowDeactivate"); }
+	// override function onWindowDropFile(file:String):Void { trace("onWindowDropFile"); }
+	// override function onWindowEnter():Void { trace("onWindowEnter"); }
+	// override function onWindowExpose():Void { trace("onWindowExpose"); }
+	// override function onWindowFocusIn():Void { trace("onWindowFocusIn"); }
+	// override function onWindowFocusOut():Void { trace("onWindowFocusOut"); }
+	// override function onWindowFullscreen():Void { trace("onWindowFullscreen"); }
+	// override function onWindowMove(x:Float, y:Float):Void { trace("onWindowMove"); }
+	// override function onWindowMinimize():Void { trace("onWindowMinimize"); }
+	// override function onWindowRestore():Void { trace("onWindowRestore"); }
+	
+}
