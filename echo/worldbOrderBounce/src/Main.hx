36cfeee6130b0bb1bdf2d2a6b55187e598ffa60b
maitag <info@maitag.de>
2025-02-23 02:42:22 +0100

    new echo sample where it bounces at the border ;)

---
+++ echo/worldbOrderBounce/src/Main.hx
@@ -0,0 +1,167 @@
+package;
+
+import haxe.CallStack;
+
+import lime.app.Application;
+import lime.ui.Window;
+
+import peote.view.*;
+
+import echo.Echo;
+import echo.math.Vector2;
+import echo.World;
+import echo.data.Options;
+
+class Main extends Application
+{
+	override function onWindowCreate():Void
+	{
+		switch (window.context.type)
+		{
+			case WEBGL, OPENGL, OPENGLES:
+				try startSample(window)
+				catch (_) trace(CallStack.toString(CallStack.exceptionStack()), _);
+			default: throw("Sorry, only works with OpenGL.");
+		}
+	}
+	
+	// ------------------------------------------------------------
+	// --------------- SAMPLE STARTS HERE -------------------------
+	// ------------------------------------------------------------	
+	
+	var world:World;
+
+	var peoteView:PeoteView;
+	var display:Display;
+
+	var red:Rectangle;
+	
+	public function startSample(window:Window)
+	{
+		peoteView = new PeoteView(window);
+
+		display = new Display(0, 0, window.width, window.height, Color.GREEN);
+		peoteView.addDisplay(display);
+		
+		
+		// ---- ECHO WORLD ------
+		
+		world = Echo.start({
+			width: 64, // Affects the bounds for collision checks.
+			height: 64, // Affects the bounds for collision checks.
+			gravity_y: 40, // Force of Gravity on the Y axis. Also available for the X axis.
+			iterations: 2 // Sets the number of Physics iterations that will occur each time the World steps.
+		});
+		
+
+		// init Rectangle Program and add it to Display
+		Rectangle.init(world);
+		Rectangle.addToDisplay(display);
+
+
+		
+		// create echo bodies
+		
+		red = new Rectangle(Color.RED, 
+			{
+				//mass: 4,
+				x: 150,
+				y: 100,
+				rotation: 45,
+				rotational_velocity:15,
+				velocity_x:-100,
+				velocity_y:-90,
+				material: {
+					// elasticity: 0.5
+					elasticity: 1.0
+				},
+				shape: {
+					type: RECT,
+					width: 200,
+					height: 100
+				}
+			}
+		);
+
+		
+		
+		var blue = new Rectangle(Color.BLUE,
+			{
+				mass: 0, // static
+				x: 400,
+				y: 300,
+				rotation: 0,
+				material: {
+					// elasticity: 0.5
+					elasticity: 1.0
+				},
+				shape: {
+					type: RECT,
+					width: 100,
+					height: 100
+				}
+			}
+		);
+		
+		
+		// let them collide
+		
+		world.listen(red.body, blue.body, {
+			separate: true, // red and blue collides
+			enter: (a, b, c) -> trace("Collision Entered"), // at first frame that a collision starts
+			//stay: (a, b, c) -> trace("Collision Stayed"), // at frames when the two Bodies are continuing to collide
+			exit: (a, b) -> trace("Collision Exited"), // at collision ends
+		});
+		
+	}
+	
+	// ------------------------------------------------------------
+	// ----------------- LIME EVENTS ------------------------------
+	// ------------------------------------------------------------	
+
+	public override function update(deltaTime:Int):Void
+	{
+		world.step(deltaTime / 1000);
+	}
+
+	// public override function render(context:lime.graphics.RenderContext):Void {}
+	// public override function onRenderContextLost ():Void trace(" --- WARNING: LOST RENDERCONTEXT --- ");
+	// public override function onRenderContextRestored (context:lime.graphics.RenderContext):Void trace(" --- onRenderContextRestored --- ");	
+
+	// ----------------- MOUSE EVENTS ------------------------------
+	// public override function onMouseMove (x:Float, y:Float):Void {}
+	// public override function onMouseDown (x:Float, y:Float, button:lime.ui.MouseButton):Void {}
+	// public override function onMouseUp (x:Float, y:Float, button:lime.ui.MouseButton):Void {}
+	// public override function onMouseWheel (deltaX:Float, deltaY:Float, deltaMode:lime.ui.MouseWheelMode):Void {}
+	// public override function onMouseMoveRelative (x:Float, y:Float):Void {}
+
+	// ----------------- TOUCH EVENTS ------------------------------
+	// public override function onTouchStart (touch:lime.ui.Touch):Void {}
+	// public override function onTouchMove (touch:lime.ui.Touch):Void	{}
+	// public override function onTouchEnd (touch:lime.ui.Touch):Void {}
+	
+	// ----------------- KEYBOARD EVENTS ---------------------------
+	// public override function onKeyDown (keyCode:lime.ui.KeyCode, modifier:lime.ui.KeyModifier):Void {}
+	// public override function onKeyUp (keyCode:lime.ui.KeyCode, modifier:lime.ui.KeyModifier):Void {}
+
+	// -------------- other WINDOWS EVENTS ----------------------------
+	public override function onWindowResize (width:Int, height:Int):Void {
+		display.width = width;
+		display.height = height;
+		Rectangle.resize(width, height);
+	}
+	// public override function onWindowLeave():Void { trace("onWindowLeave"); }
+	// public override function onWindowActivate():Void { trace("onWindowActivate"); }
+	// public override function onWindowClose():Void { trace("onWindowClose"); }
+	// public override function onWindowDeactivate():Void { trace("onWindowDeactivate"); }
+	// public override function onWindowDropFile(file:String):Void { trace("onWindowDropFile"); }
+	// public override function onWindowEnter():Void { trace("onWindowEnter"); }
+	// public override function onWindowExpose():Void { trace("onWindowExpose"); }
+	// public override function onWindowFocusIn():Void { trace("onWindowFocusIn"); }
+	// public override function onWindowFocusOut():Void { trace("onWindowFocusOut"); }
+	// public override function onWindowFullscreen():Void { trace("onWindowFullscreen"); }
+	// public override function onWindowMove(x:Float, y:Float):Void { trace("onWindowMove"); }
+	// public override function onWindowMinimize():Void { trace("onWindowMinimize"); }
+	// public override function onWindowRestore():Void { trace("onWindowRestore"); }
+	
+}
---
+++ echo/worldbOrderBounce/src/Rectangle.hx
@@ -0,0 +1,129 @@
+package;
+
+import peote.view.Display;
+import peote.view.Program;
+import peote.view.Buffer;
+import peote.view.Element;
+import peote.view.Color;
+
+import echo.World;
+import echo.Body;
+import echo.math.Vector2;
+import echo.data.Options.BodyOptions;
+
+class Rectangle implements Element
+{
+	// position in pixel (relative to upper left corner of Display)
+	//@posX @anim("Position") public var x:Float = 0.0;
+	//@posY @anim("Position") public var y:Float = 0.0;
+	@posX @set("Position") public var x:Float = 0.0;
+	@posY @set("Position") public var y:Float = 0.0;
+	
+	// size in pixel
+	@sizeX public var width:Float;
+	@sizeY public var height:Float;
+	
+	// rotation around pivot point
+	@rotation public var rotation:Float = 0.0;
+	
+	// pivot x (rotation offset)
+	@pivotX public var pivotX:Float = 0.0;
+
+	// pivot y (rotation offset)
+	@pivotY public var pivotY:Float = 0.0;
+	
+	// color (RGBA)
+	@color public var color:Color = 0x000000ff;
+	
+	// z-index
+	// @zIndex public var z:Int = 0;	
+	
+	// -------------------------------------------------------
+	static var buffer:Buffer<Rectangle>;
+	static var program:Program;
+
+	static var world:World;
+
+	public static function init(world:World) {
+		Rectangle.world = world;
+		buffer = new Buffer<Rectangle>(1024, 1024, true);
+		program = new Program(buffer);
+	}
+
+	public static function addToDisplay(display:Display) display.addProgram(program);
+	public static function removeFromDisplay(display:Display) display.removeProgram(program);
+
+	public static var borderWidth:Int = 800;
+	public static var borderHeight:Int = 600;
+	public static function resize(width:Int, height:Int) {
+		borderWidth = width;
+		borderHeight = height;
+	}
+
+	
+	// -------------------------------------------------------
+	public var body:Body; // <- bridge to the echo-physic body
+	
+	public function new(color:Color, options:BodyOptions)
+	{
+		this.color = color;
+		
+		width = options.shape.width;
+		height = options.shape.height;
+		
+		pivotX = width / 2;
+		pivotY = height / 2;
+
+		x = options.x;// - pivotX;
+		y = options.y;// - pivotY;
+		
+		if (options.rotation != null) rotation = options.rotation;
+		
+		body = world.make(options);
+		
+		body.on_move = _onMove;
+		body.on_rotate = _onRotate;
+		
+		buffer.addElement(this);
+	}
+	
+	function _onMove( x:Float, y:Float)
+	{
+		setPosition(x, y);
+		
+
+		// --------------- bounce at border ----------------
+		if (body.velocity.x < 0) {
+			if (body.x - body.bounds().width/2.0 < 0) {
+				body.velocity = new Vector2(-body.velocity.x, body.velocity.y);
+				body.x = body.bounds().width/2.0;
+			}
+		}
+		else if (body.x + body.bounds().width/2.0 > borderWidth) {
+			body.velocity = new Vector2(-body.velocity.x, body.velocity.y);
+			body.x = borderWidth - body.bounds().width/2.0;
+		}
+
+		if (body.velocity.y < 0) {
+			if (body.y - body.bounds().height/2.0 < 0) {
+				body.velocity = new Vector2(body.velocity.x, -body.velocity.y);
+				body.y = body.bounds().height/2.0;
+			}
+		}
+		else if (body.y + body.bounds().height/2.0 > borderHeight) {
+			body.velocity = new Vector2(body.velocity.x, -body.velocity.y);
+			body.y = borderHeight - body.bounds().height/2.0;
+		}
+
+
+		buffer.updateElement(this);
+	}
+	
+	function _onRotate(rotation:Float) 
+	{
+		//trace("onRotate", rotation);
+		this.rotation = rotation;
+		buffer.updateElement(this);
+	}
+	
+}
