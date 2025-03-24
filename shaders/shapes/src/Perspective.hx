dc510eac347123093fdcf891513f97a3f02bbe33
maitag <info@maitag.de>
2025-03-24 23:15:38 +0100

    dirty webgl-hack to get the shapes perspective shader run in browser also

--- shaders/shapes/src/Perspective.hx
+++ shaders/shapes/src/Perspective.hx
@@ -12,7 +12,6 @@ class Perspective implements Element
 	@custom @const @formula("(mouseX-0.5)*800.0") var tilt:Int;
 	@posX @formula("x + tilt*(1.0-aPosition.y)") public var x:Int;
 
-
 	@posY public var y:Int;
 	
 	@sizeY public var h:Int; // height
@@ -23,12 +22,20 @@ class Perspective implements Element
 	
 	@sizeX @formula("w - (w-(w*tip))*(1.0-aPosition.y)") public var w:Int; // width
 
-
 	// REMAP the ORIGIN QUAD texture-COORDS:
+	#if html5
+	// dirty webgl-HACK to avoid "flat"
+	@custom @varying @const @formula("0.0; webglTexCoordX = (aPosition.x * size.x - pivot.x)/aSize.x") var texCoordX:Float;
+	#else
 	@custom @varying @const @formula("(aPosition.x * size.x - pivot.x)/aSize.x") var texCoordX:Float;
+	#end
 
 	@pivotX @const @formula("w/2.0") var px:Int;
 	@pivotY @const @formula("h/2.0") var py:Int;
+
+
+	// @rotation @const @formula("rotation") var r:Float;
+
 	
 	static public var buffer:Buffer<Perspective>;
 	static public var program:Program;
@@ -37,29 +44,48 @@ class Perspective implements Element
 	
 	static public function init(uniforms:Array<UniformFloat>, display:Display) {	
 		buffer = new Buffer<Perspective>(1, 1, true);
-		program = new Program(buffer);		
-		program.injectIntoVertexShader(uniforms);
+		program = new Program(buffer);
+
+		if (PeoteGL.Version.isES3) {
+			program.injectIntoVertexShader("out float webglTexCoordX;", uniforms); // dirty webgl-HACK to avoid "flat"
+		}
+		else program.injectIntoVertexShader(uniforms);
+
 		display.addProgram(program);
 		
 		// load grid test-image:
 		Loader.image("assets/grid.png", true, function (image:Image) 
 		{
 			var texture = new Texture(image.width, image.height);
-			texture.setSmooth(true,true);
+			texture.setSmooth(false,false);
 			texture.setData(image);				
 			program.setTexture(texture);
+			
+			program.injectIntoFragmentShader('
+			${(PeoteGL.Version.isES3) ? "in float webglTexCoordX;" : ""} // dirty webgl-HACK
 
-			program.injectIntoFragmentShader("
 			vec4 transformTexture(int textureID, float texCoordX, float tip) {				
 				
-				texCoordX = 0.5 + texCoordX / mix(tip, 1.0, vTexCoord.y);
+				// dirty webgl-HACK to avoid "flat"
+				${(PeoteGL.Version.isES3) ? "
+					return getTextureColor( textureID, vec2(0.5 + webglTexCoordX / mix(tip, 1.0, vTexCoord.y), vTexCoord.y ));
+					" 
+					: 
+					"
+					texCoordX = 0.5 + texCoordX / mix(tip, 1.0, vTexCoord.y);
+					return getTextureColor( textureID, vec2(texCoordX, vTexCoord.y ));
+					"
+				} 
 				
-				return getTextureColor( textureID, vec2(texCoordX, vTexCoord.y ));
+				
+				
 
 				// to let texture scale smaller on trapezoids tip
 				// return getTextureColor( textureID, vec2(texCoordX, pow(vTexCoord.y,tip) ));
 			}
-			", uniforms);
+			'
+			// ,uniforms // TAKE CARE, on webgl sometimes the same uniform can not shared between vertex and fragment shader
+			);
 			program.setColorFormula("transformTexture(default_ID, texCoordX, tip)");
 
 		});
