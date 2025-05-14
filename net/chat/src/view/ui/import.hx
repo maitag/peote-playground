53e344563a041e4f47ac6b6ca924f1e609c0cfc2
maitag <info@maitag.de>
2025-05-14 23:57:34 +0200

    fixing lettercase ... now its import

---
+++ net/chat/src/view/ui/import.hx
@@ -0,0 +1,35 @@
+package view.ui;
+
+import lime.ui.FileDialog;
+import lime.ui.FileDialogType;
+
+import peote.view.Color;
+
+import view.ui.Fnt;
+import view.ui.Fnt.TextLine;
+import view.ui.Fnt.TextPage;
+
+import peote.ui.PeoteUIDisplay;
+
+import peote.ui.interactive.UIElement;
+import peote.ui.interactive.UIArea;
+import peote.ui.interactive.UISlider;
+import peote.ui.interactive.interfaces.ParentElement;
+
+import peote.ui.style.BoxStyle;
+import peote.ui.style.RoundBorderStyle;
+import peote.ui.style.FontStyleTiled;
+import peote.ui.style.interfaces.Style;
+
+import peote.ui.config.TextConfig;
+import peote.ui.config.AreaConfig;
+import peote.ui.config.SliderConfig;
+import peote.ui.config.ResizeType;
+import peote.ui.config.Space;
+import peote.ui.config.HAlign;
+import peote.ui.config.VAlign;
+
+
+import peote.ui.event.PointerEvent;
+import peote.ui.event.WheelEvent;
+
