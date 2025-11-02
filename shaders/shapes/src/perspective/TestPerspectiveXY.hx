e28ee43d9077b1250f5e71410b77f6273aa623ba
maitag <info@maitag.de>
2025-11-02 17:31:55 +0100

    trying size-fixes for both rotation-axis-changes, but still problem into calculation

--- shaders/shapes/src/perspective/TestPerspectiveXY.hx
+++ shaders/shapes/src/perspective/TestPerspectiveXY.hx
@@ -68,9 +68,11 @@ class TestPerspectiveXY extends Application
 		element.update();		
 	}
 	
+	var isShift = false;
 	override function onKeyDown(code:KeyCode, modifier:KeyModifier):Void
 	{
 		switch code {
+			case LEFT_SHIFT: isShift = true;
 			case RIGHT:
 				element.tipX *= 1.05;
 			case LEFT:
@@ -85,7 +87,8 @@ class TestPerspectiveXY extends Application
 				element.w = 400;
 				element.h = 300;
 				element.r = 0;
-				rotation = 0;
+				rotationX = 0;
+				rotationY = 0;
 			case NUMPAD_6: element.px += 0.1;
 			case NUMPAD_4: element.px -= 0.1;
 			case NUMPAD_8: element.py += 0.1;
@@ -100,21 +103,46 @@ class TestPerspectiveXY extends Application
 		// element.w = 400*Math.cos( (Math.abs(element.tipX-1))*Math.PI/1.0 );
 		element.update();
 	}
-	
-	var rotation:Float = 0.0;
+	override function onKeyUp (code:KeyCode, modifier:KeyModifier):Void {
+		switch code {
+			case LEFT_SHIFT: isShift = false;
+			case _:
+		}
+	}
+
+	var rotationX:Float = 0.0;
+	var rotationY:Float = 0.0;
 
 	override function onMouseWheel (deltaX:Float, deltaY:Float, deltaMode:lime.ui.MouseWheelMode):Void {
 		if (element == null) return;
 
-		rotation = ( rotation + 5 * ((deltaY>0)?1:-1) ) % 360;
-		if (rotation < 0) rotation = 360 + rotation; 
-		trace(rotation);
+		if (isShift)
+		{
+			rotationX = ( rotationX + 5 * ((deltaY>0)?1:-1) ) % 360;
+			if (rotationX < 0) rotationX = 360 + rotationX;
 
-		element.tipX = 1 + Math.abs(  Math.sin( rotation/360 * Math.PI *2 ) );
+			element.tipY = 1 + Math.abs(  Math.sin( rotationX/360 * Math.PI *2 ) );
+
+			element.h = 300 * Math.cos( rotationX/360 * Math.PI *2) ;
+			element.w = 400 / (1+(element.tipY-1)*0.5);
+		} 
+		else
+		{
+			rotationY = ( rotationY + 5 * ((deltaY>0)?1:-1) ) % 360;
+			if (rotationY < 0) rotationY = 360 + rotationY; 
+
+			element.tipX = 1 + Math.abs(  Math.sin( rotationY/360 * Math.PI *2 ) );
+
+			element.w = 400 * Math.cos( rotationY/360 * Math.PI *2) ;
+			element.h = 300 / (1+(element.tipX-1)*0.5);
+		}
+
+		trace(rotationX, rotationY);
+
+		// TODO: put both size-fixes together (not working yet):
+		// element.w = 400 * Math.cos( rotationY/360 * Math.PI *2)  / (1+(element.tipY-1)*0.5);
+		// element.h = 300 * Math.cos( rotationX/360 * Math.PI *2)  / (1+(element.tipX-1)*0.5);
 		
-		element.w = 400 * Math.cos( rotation/360 * Math.PI *2) ;
-		// element.h = 300 / element.tipX; // rotate about left side
-		element.h = 300 / (1+(element.tipX-1)*0.5);
 
 		element.update();
 	}
